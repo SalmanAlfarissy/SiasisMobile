02066d3a725c3812f79763a507fe092e998cbd81
Salman Alfarissy <salman.alfarissy26@gmail.com>
2021-06-03 15:42:49 +0700

    second commit

--- app/src/main/java/com/pnp/siasismobile/LoginActivity.java
+++ app/src/main/java/com/pnp/siasismobile/LoginActivity.java
@@ -1,14 +1,131 @@
 package com.pnp.siasismobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
+    final String URL_SIGNIN = "http://192.168.56.1/adm_siasis/backend/login_siswa.php";
+    EditText txtuserid;
+    EditText txtpasswd;
+    CheckBox psshow;
+    Context context;
+    CardView btnLogin;
+    String idsiswa,passwd;
+    String user,pass;
+    RequestQueue requestQueue;
+    private StringRequest stringRequest;
+
+    ArrayList<HashMap<String,String>> list_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        context = LoginActivity.this;
+
+        //Initializing views
+        txtuserid =findViewById(R.id.txtuserid);
+        txtpasswd =findViewById(R.id.txtpasswd);
+        psshow =findViewById(R.id.psshow);
+
+        btnLogin =findViewById(R.id.btnlogin);
+        requestQueue = Volley.newRequestQueue(LoginActivity.this);
+
+        list_data = new ArrayList<HashMap<String, String>>();
+
+        psshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (psshow.isChecked()){
+                    txtpasswd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else {
+                    txtpasswd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+        stringRequest = new StringRequest(Request.Method.GET, URL_SIGNIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONArray jsonArray = jsonObject.getJSONArray("siswa");
+                    for (int a = 0; a < jsonArray.length(); a++) {
+                        JSONObject json = jsonArray.getJSONObject(a);
+                        HashMap<String, String> map = new HashMap<String, String>();
+                        map.put("nis_siswa", json.getString("nis"));
+                        map.put("pass", json.getString("password"));
+                        list_data.add(map);
+                    }
+                    user = list_data.get(0).get("nis_siswa");
+                    pass = list_data.get(0).get("pass");
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this,error.getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        requestQueue.add(stringRequest);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                idsiswa = txtuserid.getText().toString();
+                passwd = txtpasswd.getText().toString();
+                if (idsiswa.equals(user)&&passwd.equals(pass)){
+                    Intent intenmenu = new Intent(LoginActivity.this,MainActivity.class);
+//                    intenmenu.putExtra("name",list_data.get(0).get("nama"));
+//                    intenmenu.putExtra("iduser",list_data.get(0).get("id"));
+                    Toast.makeText(LoginActivity.this,"Login Sukses",Toast.LENGTH_LONG).show();
+                    startActivity(intenmenu);
+                    ClearText();
+
+                }else{
+                    Toast.makeText(LoginActivity.this,"Login Gagal\n Please Try Again!!",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
     }
+
+    private void ClearText() {
+        txtuserid.setText("");
+        txtpasswd.setText("");
+    }
+
 }
---
+++ app/src/main/java/com/pnp/siasismobile/ui/sekolah/SekolahFragment.java
@@ -0,0 +1,50 @@
+package com.pnp.siasismobile.ui.sekolah;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.pnp.siasismobile.R;
+
+
+public class SekolahFragment extends Fragment {
+
+    public SekolahFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sekolah, container, false);
+        TextView vimi = view.findViewById(R.id.vimi);
+        TextView judul = view.findViewById(R.id.judul);
+        judul.setText("Sistem Akademik Siswa\n\n");
+        vimi.setText("VISI DAN MISI\n\n" +
+                "VISI :\n" +
+                "\t\t\tBERIMAN, CERDAS, KOMPETITIF, BERBUDAYA SERTA BERWAWASAN LINGKUNGAN\n\n" +
+                "MISI :\n"+
+                "1.MENGHAYATI AJARAN AGAMA SECARA " +
+                "  MENDALAM MELALUI PEMBELAJARAN DAN " +
+                "  PEMBIASAAN SERTA MENJAGA KELESTARIAN " +
+                "  LINGKUNGAN CIPTAAN TUHAN.\n\n" +
+                "2.MENGEMBANGKAN POTENSI KECERDASAN " +
+                "  INTELEKTUAL, EMOSIONAL DAN SPRITUAL YANG " +
+                "  RAMAH LINGKUNGAN\n\n" +
+                "3.MAMPU BERKOMPETENSI DIBIDANG " +
+                "  AKADEMIK DAN NON AKADEMIK DITINGKAT " +
+                "  DAERAH MAUPUN NASIONAL\n\n" +
+                "4.MENJUNJUNG TINGGI NLAI - NILAI " +
+                "  KEMANUSIAN, TEGUH DALAM PENDIRIAN, " +
+                "  PEDULI LINGKUNGAN DAN MENGHARGAI " +
+                "  PERBEDAAN DENGAN JIWA\n\n" +
+                "5.MEWUJUDKAN SEKOLAH BERWAWASAN " +
+                "  LINGKUNGAN HIDUP\n\n");
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
